bfe8df28f97300ae4c7c100e2af59ad82e7c0261
mesropt <mesroptnamak@gmail.com>
2021-09-19 22:31:44 +0300

    added test in urls.py, views.py and commented it

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from mainapp.views import index, products, test
+from django.urls import path, include
+from mainapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
-#    path('test/', test, name='test'),
+    path('products/', include('mainapp.urls', namespace='mainapp')),
 ]
