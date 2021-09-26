fb66e769ad2304a6faf1cde51f4f7d9943eb06f3
mesropt <mesroptnamak@gmail.com>
2021-09-26 11:43:07 +0300

    added show message for template

--- users/views.py
+++ users/views.py
@@ -16,8 +16,6 @@ def login(request):
             if user.is_active:
                 auth.login(request,user)
                 return HttpResponseRedirect(reverse('index'))
-        else:
-            print(form.errors)
     else:
         form = UserLoginForm()
     context = {
@@ -33,8 +31,6 @@ def register(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:login'))
-        else:
-            print(form.errors)
     else:
         form = UserRegisterForm()
     context = {
