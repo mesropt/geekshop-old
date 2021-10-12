4ae10d643820438b365cd612080358d505d27ccf
mesropt <mesroptnamak@gmail.com>
2021-10-12 18:49:38 +0300

    added admins app

---
+++ admins/views.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+from geekshop.mixin import CustomDispatchMixin
+from users.models import User
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+def index(request):
+    return render(request,'admins/admin.html')
+
+class UserListView(ListView,CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    context_object_name = 'users'
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+       context = super(UserListView, self).get_context_data(**kwargs)
+       context['title'] = 'Админка | Пользователи'
+       return context
+
+class UserCreateView(CreateView,CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+       context = super(UserCreateView, self).get_context_data(**kwargs)
+       context['title'] = 'Админка | Регистрация'
+       return context
+
+
+class UserUpdateView(UpdateView,CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+       context = super(UserUpdateView, self).get_context_data(**kwargs)
+       context['title'] = 'Админка | Обновление пользователя'
+       return context
+
+
+class UserDeleteView(DeleteView):
+
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    success_url = reverse_lazy('admins:admins_user')
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
